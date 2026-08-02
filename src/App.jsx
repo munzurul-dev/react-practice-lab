fdb2e2971dfb43de45e6503d1e8ac4d88a82e294
Muhammad Munzurul <muhammadmunzurul@gmail.com>
2026-08-02 22:29:40 +0600

    Add Recharts data visualization

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,18 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import { Suspense, useEffect, useState } from "react";
 import AllPricingCard from "./Components/AllPricingCard/AllPricingCard";
-
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  CartesianGrid,
+  ResponsiveContainer,
+} from "recharts";
 function App() {
+  const [data, setData] = useState([]);
   const [navData, setNavData] = useState([]);
   const [pricingCardData, setPricingCardData] = useState([]);
   useEffect(() => {
@@ -13,11 +23,16 @@ function App() {
     });
   }, []);
   useEffect(() => {
+    axios.get("/ReChart.json").then((UseData) => {
+      setData(UseData.data);
+    });
+  }, []);
+  useEffect(() => {
     axios.get("/Navbar.json").then((response) => {
       setNavData(response.data);
     });
   }, []);
-
+  console.log(data);
   return (
     <>
       <div>
@@ -25,6 +40,19 @@ function App() {
         <Suspense fallback={<h2>Loading...</h2>}>
           <AllPricingCard pricingCardData={pricingCardData}></AllPricingCard>
         </Suspense>
+        <div>
+          <ResponsiveContainer width="100%" height="400">
+            <BarChart data={data}>
+              <CartesianGrid strokeDasharray="3 3"></CartesianGrid>
+              <XAxis dataKey="month"></XAxis>
+              <YAxis></YAxis>
+              <Legend></Legend>
+              <Tooltip></Tooltip>
+              <Bar dataKey="sales" fill="#3b82f6" name="sales"></Bar>
+              <Bar dataKey="orders" fill="#10b981" name="Orders" />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
       </div>
     </>
   );
